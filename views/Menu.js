8abf3b8445e0aacaf4bf660182c358489090842d
francijpg <hellofrancijpg@gmail.com>
2021-04-30 09:52:39 -0400

    move files to new src directory

--- views/Menu.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import {Text} from 'react-native';
-
-const Menu = () => {
-  return <Text>Menu</Text>;
-};
-
-export default Menu;
--- views/NewOrder.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import {Text} from 'react-native';
-
-const NewOrder = () => {
-  return <Text>New Order</Text>;
-};
-
-export default NewOrder;
