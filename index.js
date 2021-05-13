88e4c0d08f24ba707e589df419889004b72d912f
francijpg <hellofrancijpg@gmail.com>
2021-05-13 08:31:14 -0400

    Update index.js

--- index.js
+++ index.js
@@ -5,5 +5,12 @@
 import {AppRegistry} from 'react-native';
 import App from './src/App';
 import {name as appName} from './app.json';
+import {LogBox} from 'react-native';
+
+// Ignore log notification by message:
+LogBox.ignoreLogs(['Warning: ...']);
+
+// Ignore all log notifications:
+LogBox.ignoreAllLogs();
 
 AppRegistry.registerComponent(appName, () => App);
