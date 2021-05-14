daa5e6c0d0a287b883b17b0385aee3f910b7c4f2
francijpg <hellofrancijpg@gmail.com>
2021-05-14 08:23:00 -0400

    Update storageReducer.js

--- src/contexts/storage/storageReducer.js
+++ src/contexts/storage/storageReducer.js
@@ -1,19 +1,37 @@
 import {
-  GET_PRODUCTS_SUCCESSFULLY,
-  GET_ONE_PRODUCT,
+  FETCH_DISHES_SUCCESSFULLY,
+  GET_DISH,
+  CONFIRM_ORDER_DISHES,
+  GET_ORDER_SUMMARY,
+  REMOVE_ORDER_DISH,
 } from '../../constants/actionTypes';
 
 export default (state, action) => {
   switch (action.type) {
-    case GET_PRODUCTS_SUCCESSFULLY:
+    case FETCH_DISHES_SUCCESSFULLY:
       return {
         ...state,
         menu: action.payload,
       };
-    case GET_ONE_PRODUCT:
+    case GET_DISH:
       return {
         ...state,
         dish: action.payload,
+      };
+    case CONFIRM_ORDER_DISHES:
+      return {
+        ...state,
+        order: [...state.order, action.payload],
+      };
+    case GET_ORDER_SUMMARY:
+      return {
+        ...state,
+        totalToPay: action.payload,
+      };
+    case REMOVE_ORDER_DISH:
+      return {
+        ...state,
+        order: state.order.filter(dish => dish.id !== action.payload),
       };
     default:
       return state;
