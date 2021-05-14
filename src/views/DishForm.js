4e672b6843ed1996afc52fa8dd9cb83c93012f3b
francijpg <hellofrancijpg@gmail.com>
2021-05-14 08:25:09 -0400

    Update DishForm.js

--- src/views/DishForm.js
+++ src/views/DishForm.js
@@ -1,8 +1,148 @@
-import React from 'react';
-import {Text} from 'react-native';
+import React, {useState, useEffect} from 'react';
+import {Alert, StyleSheet} from 'react-native';
+import {
+  Container,
+  Content,
+  Form,
+  Icon,
+  Input,
+  Grid,
+  Col,
+  Button,
+  Text,
+  Footer,
+  FooterTab,
+} from 'native-base';
+import {useNavigation} from '@react-navigation/native';
+import globalStyles from '../styles/global';
+import {useStorage} from '../contexts/storage/storageContext';
 
 const DishForm = () => {
-  return <Text>Dish Form</Text>;
+  const [quantity, setQuantity] = useState(1);
+  const [total, setTotal] = useState(0);
+  const {dish, setOrder} = useStorage();
+  const {price} = dish;
+
+  const navigation = useNavigation();
+
+  useEffect(() => {
+    calculateTotal();
+  }, [calculateTotal, quantity]);
+
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  const calculateTotal = () => {
+    const totalToPay = price * quantity;
+    setTotal(totalToPay);
+  };
+
+  const decreaseOne = () => {
+    if (quantity > 1) {
+      const newQuantity = Number(quantity) - 1;
+      setQuantity(newQuantity);
+    }
+  };
+
+  const increaseOne = () => {
+    const newQuantity = Number(quantity) + 1;
+    setQuantity(newQuantity);
+  };
+
+  const confirmOrder = () => {
+    const order = {
+      ...dish,
+      amount: quantity,
+      total,
+    };
+
+    setOrder(order);
+    navigation.navigate('OrderSummary');
+    // Alert.alert(
+    //   'Do you want to confirm your order?',
+    //   'A confirmed order can no longer be modified',
+    //   [
+    //     {
+    //       text: 'Confirm',
+    //       onPress: () => {
+    //         const order = {
+    //           ...dish,
+    //           amount: quantity,
+    //           total,
+    //         };
+
+    //         setOrder(order);
+    //         navigation.navigate('OrderSummary');
+    //       },
+    //     },
+    //     {
+    //       text: 'Cancel',
+    //       style: 'cancel',
+    //     },
+    //   ],
+    // );
+  };
+
+  return (
+    <Container>
+      <Content>
+        <Form>
+          <Text style={globalStyles.title}>Amount</Text>
+          <Grid>
+            <Col>
+              <Button
+                props
+                dark
+                style={styles.button}
+                onPress={() => decreaseOne(quantity)}>
+                <Icon style={styles.icon} name="remove" />
+              </Button>
+            </Col>
+            <Col>
+              <Input
+                style={styles.input}
+                value={quantity.toString()}
+                keyboardType="numeric"
+                onChangeText={cantidad => setQuantity(cantidad)}
+              />
+            </Col>
+            <Col>
+              <Button
+                props
+                dark
+                style={styles.button}
+                onPress={() => increaseOne()}>
+                <Icon style={styles.icon} name="add" />
+              </Button>
+            </Col>
+          </Grid>
+
+          <Text style={globalStyles.amount}>Subtotal: $ {total} </Text>
+        </Form>
+      </Content>
+
+      <Footer>
+        <FooterTab>
+          <Button style={globalStyles.button} onPress={() => confirmOrder()}>
+            <Text style={globalStyles.buttonText}>Add to Order</Text>
+          </Button>
+        </FooterTab>
+      </Footer>
+    </Container>
+  );
 };
+
+const styles = StyleSheet.create({
+  button: {
+    height: 80,
+    justifyContent: 'center',
+    width: '100%',
+  },
+  input: {
+    textAlign: 'center',
+    fontSize: 20,
+  },
+  icon: {
+    fontSize: 40,
+  },
+});
 
 export default DishForm;
